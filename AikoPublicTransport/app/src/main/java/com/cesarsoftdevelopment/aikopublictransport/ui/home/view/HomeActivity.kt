eec4fef7ed6e81134cbcbe72c76975766235e499
Cesar <76631787+CesarSoftDevelopment@users.noreply.github.com>
2024-08-23 15:32:56 -0300

    Implementing logic to navigate between fragments

--- AikoPublicTransport/app/src/main/java/com/cesarsoftdevelopment/aikopublictransport/ui/home/view/HomeActivity.kt
+++ AikoPublicTransport/app/src/main/java/com/cesarsoftdevelopment/aikopublictransport/ui/home/view/HomeActivity.kt
@@ -4,24 +4,37 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.cesarsoftdevelopment.aikopublictransport.R
+import com.cesarsoftdevelopment.aikopublictransport.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setUpSplashScreen()
-        setContentView(R.layout.activity_home)
+        setUpViews()
+        setUpNavigationBar()
+    }
+
+    private fun setUpViews() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = navHostFragment.navController
     }
 
     private fun setUpSplashScreen() {
-
         val splashScreen = installSplashScreen()
-
         splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
             val splashScreenView = splashScreenViewProvider.view
-
             val fadeOut = ObjectAnimator.ofFloat(
                 splashScreenView,
                 "alpha",
@@ -29,14 +42,28 @@ class HomeActivity : AppCompatActivity() {
             ).apply {
                 duration = 500L
             }
-
             fadeOut.start()
-
             fadeOut.addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator) {
                     splashScreenViewProvider.remove()
                 }
             })
+        }
+    }
+
+    private fun setUpNavigationBar() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.mapsFragment -> {
+                    navController.navigate(R.id.mapsFragment)
+                    true
+                }
+                R.id.busLinesFragment -> {
+                    navController.navigate(R.id.busLinesFragment)
+                    true
+                }
+                else -> false
+            }
         }
     }
 
